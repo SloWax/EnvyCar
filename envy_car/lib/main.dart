40f1b06355b846baedad337d68b17a7f1593eeec
SloWax <rjsnrl0128@naver.com>
2024-08-17 01:27:55 +0900

    app package

--- envy_car/lib/main.dart
+++ envy_car/lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: '오일갈쟈',
         theme: ThemeData(
             brightness: Brightness.dark,
